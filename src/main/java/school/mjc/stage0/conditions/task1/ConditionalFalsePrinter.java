d4e5d993f858d10c1616741fe17b0d901c304103
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-10 21:01:10 +0500

    Task #1. Simple If Conditions

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean isNegative = false;
+        if (!isNegative) {
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean isPosotove = true;
+        if (isPosotove) {
+            System.out.println("true");
+        }
     }
 }
