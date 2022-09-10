1d2ad18129244aabd527ed26907265bfaf873c3e
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-10 21:16:21 +0500

    Task #1. Fixes

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -3,7 +3,9 @@ package school.mjc.stage0.conditions.task1;
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
         boolean isNegative = false;
-        if (!isNegative) {
+        if (isNegative) {
+            System.out.println("true");
+        } else {
             System.out.println("false");
         }
     }
